eb806d31cb1c2913404a0c4a6f11805036c0f033
ElCondeDeMonteCrypto <elcondedemontecrypto@gmail.com>
2021-12-25 10:22:35 -0600

    getBalance endpoint fix

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ window.onload = function() {
 
 async function isAccountEligible(account) {
     // let url = new URL('http://localhost:3000/getBalance'); // local environment
-    let url = new URL('http://3.227.225.160/getBalance'); // prod
+    let url = new URL('https://3.227.225.160/getBalance'); // prod
     let params = {addressToCheck:account};
 
     url.search = new URLSearchParams(params).toString();
