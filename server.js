d96e2497df9d02f26d0790302570b416c565b0c4
ElCondeDeMonteCrypto <elcondedemontecrypto@gmail.com>
2021-12-24 18:55:10 -0600

    added ssl

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const url = require('url');
 const port = 8080;
 
@@ -9,6 +11,7 @@ const queryString = require('query-string');
 const fshibbyController = require('./contracts/fshibby.controller');
 
 let app = express();
+app.options = { }
 app.use(express.static(__dirname)); // renders index.html
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,6 +31,11 @@ app.get('/getBalance', async function(req, res){
     }
   });
 
-app.listen(port, () => {
-    console.log('App listening on http://localhost:' + port);
-});
+https.createServer({
+    key: fs.readFileSync('./ssl/private.key'),
+    cert: fs.readFileSync('./ssl/certificate.crt')
+}, app).listen(port);
+
+// app.listen(port, () => {
+//     console.log('App listening on http://localhost:' + port);
+// });
