e458c2e1dd032d1eb551630359a8a3168743c3ed
Findshibby <91229373+Findshibby@users.noreply.github.com>
2021-12-29 20:01:38 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ window.onload = function() {
 };
 
 async function isAccountEligible(account) {
-    
+    // let url = new URL('http://localhost:8080/getBalance'); // local environment
     let url = new URL('https://xmas-fshibby-vg5sg.ondigitalocean.app/getBalance'); // prod
     let params = {addressToCheck:account};
 
